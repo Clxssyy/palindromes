00248e6bd67596abff2755d2799e28f8efbd5c25
Michael Connolly <mpc63@uakron.edu>
2023-02-16 00:02:41 -0500

    Add comments to stack functions

--- stack.hpp
+++ stack.hpp
@@ -15,26 +15,32 @@ private:
     LinkedList<T> list;
 
 public:
+    // Constructor
     Stack() {
         LinkedList<T> list;
     }
 
+    // Deconstructor
     ~Stack() {
-
+        // Should use LinkedList deconstructor
     }
 
+    // Push a value onto the stack
     void push(T value) {
         list.push(value);
     }
 
+    // Pop a value off the stack
     T pop() {
         return list.pop();
     }
 
+    // Print out the values on the stack
     void print() {
         list.print();
     }
 
+    // Check if the stack is empty
     bool isEmpty() {
         return list.isEmpty();
     }
